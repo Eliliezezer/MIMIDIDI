7b6e1c494c48c7c6597866fa7f6f868de38630f5
Eliliezezer <132705010+Eliliezezer@users.noreply.github.com>
2023-11-16 13:45:43 -0300

    sounds

--- main.js
+++ main.js
@@ -19,5 +19,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = 'som_${instrumento}'; //template string
+    const idAudio = '#som_${instrumento}'; //template string
+
+    tecla.onclick = function ()  {
+        tocaSom(idAudio);
+    }
+
 }
